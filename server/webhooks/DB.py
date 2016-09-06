4f1db3985330a0f382a8bdd78a8227648aca2f70
Mark Duppenthaler <mduppes@gmail.com>
2016-09-06 03:46:04 +0000

    running server

--- server/webhooks/DB.py
+++ server/webhooks/DB.py
@@ -1,37 +1,77 @@
 import mysql.connector
 import config
+import json
+import logging
+
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 def save_webhooks_update(update):
     """Given a string update, decodes it to json and stores it if valid"""
-    connection = mysql.connector.connect(**config.mysql)
-    cursor = connection.cursor()
 
+    topic = update['object']
+    entries = update['entry']
+
+    if not topic or not entries:
+        return
+
+    connection = mysql.connector.connect(**config.mysql)
+    cursor = connection.cursor(buffered=True)
+    
+    for entry in entries:
+        fat_ping = 'changes' in entry 
+        target_id = entry['id']
+        time = entry['time']
+        
+        if fat_ping:
+            for change in entry['changes']:
+                value = json.dumps(change)
+                field = change['field']
+                insert_webhooks_update(cursor, target_id, topic, time, field, value)
+        else:
+            for field in entry['changed_fields']:
+                insert_webhooks_update(cursor, target_id, topic, time, field, None)
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    
+def insert_webhooks_update(
+    cursor,
+    target_id,
+    topic,
+    time,
+    field,
+    value):
     query = """INSERT into webhooks
     (target_id, topic, field, time, fat, value)
     VALUES
-    ("707499519337800", "page", "feed", 1473119394, 1, '{"parent_id":"707499519337800_1069771549777260","sender_name":"GrumpyTravels","sender_id":707499519337800,"item":"like","verb":"add","created_time":1473119394,"post_id":"707499519337800_1069771549777260"}')
+    (%s, %s, %s, %s, %s, %s)
     """
-
-    cursor.execute(query)
-    connection.commit()
-
-    cursor.close()
-    connection.close()
+    logging.info('{}{}{}{}{}{}'.format(target_id, topic, field, time, value is not None, value))
+    cursor.execute(query, (target_id, topic, field, time, int(value is not None), value))
 
 def get_webhooks_updates():
     connection = mysql.connector.connect(**config.mysql)
     cursor = connection.cursor()
 
-    query = """SELECT target_id, topic, field, time, fat, value from webhooks"""
+    query = """SELECT target_id, topic, field, time, fat, value from webhooks order by time desc limit 10"""
 
 
     cursor.execute(query)
 
-    for item in cursor:
-      print("{}".format(item))
+    items = []
+    for (target_id, topic, field, time, fat_ping, value) in cursor:
+      items.append({
+        'target_id': target_id,
+        'topic': topic,
+        'field': field,
+        'time': time,
+        'fat_ping': fat_ping,
+        'value': json.loads(value)
+      })
 
     cursor.close()
     connection.close()
+    return items
 
-
-get_webhooks_updates()
--- server/webhooks/app.py
+++ server/webhooks/app.py
@@ -1,15 +1,15 @@
 import logging
+import DB
+import json
 from flask import Flask, request
+
+logging.basicConfig(filename='debug.log',level=logging.DEBUG)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    contents = request.args.get('yo')
-    logging.info(contents)
-    if not contents:
-        contents = ''
-    return contents
+    return json.dumps(DB.get_webhooks_updates(), indent=4, separators=(',', ': '))
 
 @app.route("/webhooks", methods=['get'])
 def webhooks_setup():
@@ -19,7 +19,20 @@ def webhooks_setup():
 def webhooks_receive():
     json = request.get_json()
     logging.info(json)
+    DB.save_webhooks_update(json)
     return ''
+
+@app.route("/webhooks_backup", methods=['get'])
+def webhooks_setup():
+    return request.args.get('hub.challenge')
+
+@app.route("/webhooks_backup", methods=['post'])
+def webhooks_receive():
+    json = request.get_json()
+    logging.info(json)
+    DB.save_webhooks_update(json)
+    return ''
+
 
 if __name__ == "__main__":
     app.run()
